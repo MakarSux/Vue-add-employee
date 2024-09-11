34955f1c3c63d59bd1587e57df4512fc678ea8f0
MakarSux <mm.pyshkin@yandex.ru>
2024-09-11 15:16:15 +0300

    working app

--- add-employe/src/App.vue
+++ add-employe/src/App.vue
@@ -1,70 +1,104 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+import * as bootstrap from 'bootstrap';
 
-import * as bootstrap from 'bootstrap'
+// Создаем реактивные переменные для хранения данных
+const items = ref([]);
+const newItem = ref({
+  tabNumber: '',
+  fullName: '',
+  email: '',
+  phone: '',
+  position: ''
+});
 
+// Функция для добавления данных
+const addData = () => {
+  items.value.push({ ...newItem.value });
+  newItem.value = {
+    tabNumber: '',
+    fullName: '',
+    email: '',
+    phone: '',
+    position: ''
+  };
+};
 </script>
 
 <template>
-  <div class="wrapper d-flex flex-nowrap">
-    <header></header>
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      <span class="fs-4">Sidebar</span>
-    </a>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-          Dashboard
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-          Customers
-        </a>
-      </li>
-    </ul>
-    <hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
+  <div class="wrapper">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+          <use xlink:href="#bootstrap"></use>
+        </svg>
+        <span class="fs-4">Simple header</span>
       </a>
-      <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
       </ul>
+    </header>
+
+    <div class="container">
+      <div class="row mb-3 text-center">
+        <div class="col-2 themed-grid-col">Таб номер</div>
+        <div class="col-3 themed-grid-col">ФИО</div>
+        <div class="col-2 themed-grid-col">email</div>
+        <div class="col-2 themed-grid-col">телефон</div>
+        <div class="col-2 themed-grid-col">должность</div>
+      </div>
+      <div class="row mb-3 text-center" v-for="(item, index) in items" :key="index">
+        <div class="col-2 themed-grid-col">{{ item.tabNumber }}</div>
+        <div class="col-3 themed-grid-col">{{ item.fullName }}</div>
+        <div class="col-2 themed-grid-col">{{ item.email }}</div>
+        <div class="col-2 themed-grid-col">{{ item.phone }}</div>
+        <div class="col-2 themed-grid-col">{{ item.position }}</div>
+      </div>
     </div>
-  </div>
+
+    <div class="container">
+      <form action="" class="form">
+        <input type="text" class="form-control" v-model="newItem.tabNumber" placeholder="Таб номер">
+        <input type="text" class="form-control" v-model="newItem.fullName" placeholder="ФИО">
+        <input type="text" class="form-control" v-model="newItem.email" placeholder="email">
+        <input type="text" class="form-control" v-model="newItem.phone" placeholder="телефон">
+        <select name="" id="" class="form-select" v-model="newItem.position">
+          <option value="Программист">Программист</option>
+          <option value="Грузчик">Грузчик</option>
+          <option value="Оператор">Оператор</option>
+        </select>
+
+        <button type="button" @click="addData" class="btn btn-primary">Сохранить</button>
+      </form>
+    </div>
+
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+      </ul>
+      <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
+    </footer>
   </div>
 </template>
 
 <style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 20px;
+}
 
+.form-control, .form-select {
+  width: 100%;
+}
 </style>
